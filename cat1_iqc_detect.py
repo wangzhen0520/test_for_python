979482fbfd99112e9415caba890cc18c36c67778
wangzhen <wanghzhenk@fotile.com>
2024-05-26 10:42:12 +0800

    1. usb连接时默认选择串口

--- cat1_iqc_detect.py
+++ cat1_iqc_detect.py
@@ -9,24 +9,25 @@ import queue
 import json
 
 
+def get_available_ports():
+    ports = serial.tools.list_ports.comports()
+    ports_list = []
+    for i in range(len(ports)):
+        comport = list(ports[i])
+        ports_list.append([i, comport[0], comport[1]])
+    return ports_list
+
+
+def show_available_ports(ports):
+    for item in ports:
+        print("%-10s %-10s %-50s" % (item[0], item[1], item[2]))
+
+
 class SerialCommunication:
     def __init__(self):
         self.recv_queue = queue.Queue()
         self.ser_receive_flag = False
-        self.run_flag = False
-
-    def get_available_ports(self):
-        ports = serial.tools.list_ports.comports()
-        ports_list = []
-        for i in range(len(ports)):
-            comport = list(ports[i])
-            ports_list.append([i, comport[0], comport[1]])
-
-            print("%-10s %-10s %-50s" % (i, comport[0], comport[1]))
-        return ports_list
-
-    def detect_serial_port(self, ser):
-        return ser.isOpen()
+        self.recv_thread_enable = False
 
     def open_serial_port(self, port_name, baud_rate):
         try:
@@ -38,6 +39,9 @@ class SerialCommunication:
             return None
 
     def close_serial_port(self, ser):
+        if ser is None:
+            return
+
         if self.recv_queue.empty() == False:
             self.recv_queue.get_nowait()
 
@@ -45,21 +49,36 @@ class SerialCommunication:
             ser.close()
             print("close serial port: %s success" % ser.port)
 
+        ser = None
+
     def send_str_data(self, ser, data):
+        if ser is None:
+            return
+
         if ser.isOpen() == False:
             return
 
-        ser.write((data + "\n").encode("utf-8"))
         print(f"Sent: {data}")
+        try:
+            ser.write((data + "\n").encode("utf-8"))
+        except serial.serialutil.SerialException:
+            print("WriteFile failed")
 
     def send_byte_data(self, ser, data):
         if len(data) == 0:
             return
+
+        if ser is None:
+            return
+
         if ser.isOpen() == False:
             return
 
         print("send: [%d] %s" % ((len(data) + 1) / 3, data))
-        ser.write(bytearray.fromhex(data))
+        try:
+            ser.write(bytearray.fromhex(data))
+        except serial.serialutil.SerialException:
+            print("WriteFile failed")
 
     def crc16(self, data: bytes) -> int:
         # 初始化crc为0xFFFF
@@ -80,7 +99,8 @@ class SerialCommunication:
         return crc
 
     def receive_data(self):
-        while self.run_flag:
+        received_data = ""
+        while self.recv_thread_enable:
             time.sleep(0.3)
             if self.ser_receive_flag == False:
                 continue
@@ -99,7 +119,12 @@ class SerialCommunication:
 
                 received_data = self.ser.read(cnt)
             except serial.serialutil.SerialException:
-                print("SerialException")
+                print("receive data SerialException")
+                self.ser_receive_flag = False
+                # time.sleep(1)
+                continue
+
+            if len(received_data) == 0:
                 continue
 
             if received_data[0] == 0xF4:
@@ -132,15 +157,25 @@ class SerialCommunication:
         self.ser = ser
         self.ser_receive_flag = True
 
-        if self.run_flag == False:
-            self.run_flag = True
+        if self.recv_thread_enable == False:
+            self.recv_thread_enable = True
             self.receive_thread = threading.Thread(target=self.receive_data)
             self.receive_thread.daemon = True
             self.receive_thread.start()
 
-    def stop_serial_threads(self):
-        # self.run_flag = False
+    def pause_recv_data(self):
+        if self.recv_queue.empty() == False:
+            self.recv_queue.get_nowait()
+
         self.ser_receive_flag = False
+
+    def resume_recv_data(self):
+        self.ser_receive_flag = True
+
+    def stop_serial_threads(self):
+        # self.recv_thread_enable = False
+        self.ser_receive_flag = False
+        self.ser = None
         # self.receive_thread.join()
 
 
@@ -148,12 +183,17 @@ class MyFrame(wx.Frame):
     def __init__(self, *args, **kw):
         super(MyFrame, self).__init__(*args, **kw)
 
-        self.cat1_ver = ""
-        self.csq = ""
-        self.task_run_flag = False
-        self.task_run = False
-        self.com1_name = ""
-        self.com2_name = ""
+        self.cat1_ver = ""  # cat1 软件版本号
+        self.csq = ""  # cat1 信号强度
+        self.imei = "" # cat1 IMEI号码
+        self.iccid = "" # cat1 ICCID号码
+        
+        self.task_run_flag = False  # 检测线程运行检测标志
+        self.task_run_enable = False  # 检测线程运行总开关
+        self.com1_name = ""  # 串口1 名称
+        self.com2_name = ""  # 串口2 名称
+        self.ser1 = None  # 串口1 对象句柄
+        self.ser2 = None  # 串口2 对象句柄
 
         self.InitUI()
 
@@ -206,7 +246,7 @@ class MyFrame(wx.Frame):
         self.label_result_text.SetFont(font_5)
         self.label_result_text.SetForegroundColour((0, 255, 0))
 
-        self.button = wx.Button(panel, label="开始检测", pos=(390, 390))
+        self.button = wx.Button(panel, label="开始检测", pos=(420, 385), size=(120, 30))
         self.button.Bind(wx.EVT_BUTTON, self.on_button_click)
 
         self.statusbar = self.CreateStatusBar()  # 创建状态栏
@@ -215,16 +255,16 @@ class MyFrame(wx.Frame):
 
         self.sc1 = SerialCommunication()
         self.sc2 = SerialCommunication()
-        self.ser_ports = self.sc1.get_available_ports()
+        self.ser_ports = get_available_ports()
+        show_available_ports(self.ser_ports)
 
-        # 创建一个只读下拉列表，可选择Linux的各种发行版本
-        self.serial_list1 = wx.StaticText(panel, label="串口1：", pos=(10, 395), size=(50, 20), style=wx.ALIGN_LEFT)
-        self.cb1 = wx.ComboBox(panel, pos=(60, 390), choices=[], style=wx.CB_READONLY)
+        # 创建串口下拉列表
+        self.serial_list1 = wx.StaticText(panel, label="串口1：", pos=(10, 395), size=(60, 30), style=wx.ALIGN_LEFT)
+        self.cb1 = wx.ComboBox(panel, pos=(70, 390), choices=[], style=wx.CB_READONLY)
         self.cb1.Clear()
         for item in self.ser_ports:
-            if not item[2].startswith("ASR"):
-                continue
-            self.cb1.Append(item[1] + "  " + item[2])
+            if item[2].startswith("ASR"):
+                self.cb1.Append(item[1] + "  " + item[2])
 
         if self.cb1.GetCount() > 0:
             self.cb1.SetSelection(0)
@@ -235,16 +275,15 @@ class MyFrame(wx.Frame):
 
         self.cb1.Bind(wx.EVT_COMBOBOX, self.OnSelect1)
 
-        self.serial_list2 = wx.StaticText(panel, label="串口2：", pos=(200, 395), size=(50, 20), style=wx.ALIGN_LEFT)
-        self.cb2 = wx.ComboBox(panel, pos=(250, 390), choices=[], style=wx.CB_READONLY)
+        self.serial_list2 = wx.StaticText(panel, label="串口2：", pos=(210, 395), size=(60, 30), style=wx.ALIGN_LEFT)
+        self.cb2 = wx.ComboBox(panel, pos=(270, 390), choices=[], style=wx.CB_READONLY)
         self.cb2.Clear()
         for item in self.ser_ports:
-            self.cb2.Append(item[1] + "  " + item[2])
-        if len(self.ser_ports) > 1:
-            self.cb2.SetSelection(1)
-        else:
-            self.cb2.SetSelection(0)
+            if not item[2].startswith("ASR"):
+                self.cb2.Append(item[1] + "  " + item[2])
+
         if self.cb2.GetCount() > 0:
+            self.cb2.SetSelection(0)
             self.com2_name = self.cb2.GetStringSelection().split(' ')[0]
             self.statusbar.SetStatusText(self.com2_name + " 未连接", 1)
         else:
@@ -256,6 +295,11 @@ class MyFrame(wx.Frame):
         print("当前选择：%s\n com2_name: %s" % (self.cb2.GetStringSelection(), self.com2_name))
 
         self.load_config()
+
+        # 自动检测串口线程
+        auto_detect_thread = threading.Thread(target=self.auto_detect_serial)
+        auto_detect_thread.daemon = True
+        auto_detect_thread.start()
 
     def OnSelect1(self, e):
         str = e.GetString()
@@ -275,6 +319,7 @@ class MyFrame(wx.Frame):
                 print("target_version: %s" % self.target_version)
         except FileNotFoundError:
             print("config.json not found")
+            wx.MessageBox("配置文件加载失败，请检查！", "错误", wx.OK | wx.ICON_ERROR)
             return None
 
     def get_cat1_imei(self):
@@ -287,7 +332,7 @@ class MyFrame(wx.Frame):
             try:
                 data_str = self.sc1.recv_queue.get(timeout=1)
             except queue.Empty:
-                return ""
+                return
 
             if data_str.find("+GSN:"):
                 data_str = data_str.split("\r\n")
@@ -305,12 +350,10 @@ class MyFrame(wx.Frame):
                     if len(imei) > 1:
                         imei = imei[1].strip()
                     print("get imei is {0}".format(imei))
-                    return imei
-                else:
-                    return ""
+                    self.imei = imei.strip()
             else:
                 print("get data is {0}".format(data_str))
-                return ""
+                return
 
     def get_cat1_iccid(self):
         if self.ser1 is not None:
@@ -322,7 +365,7 @@ class MyFrame(wx.Frame):
             try:
                 data_str = self.sc1.recv_queue.get(timeout=1)
             except queue.Empty:
-                return ""
+                return
 
             if data_str.find("+ICCID:"):
                 data_str = data_str.split("\r\n")
@@ -336,10 +379,10 @@ class MyFrame(wx.Frame):
             if (result == "OK"):
                 iccid = iccid.split("+ICCID:")[1]
                 print("get iccid is {0}".format(iccid))
-                return iccid.strip()
+                self.iccid = iccid.strip()
             else:
                 print("get data is {0}".format(data_str))
-                return ""
+                return
 
     def get_cat1_version(self):
         if self.ser2 is not None:
@@ -368,7 +411,7 @@ class MyFrame(wx.Frame):
 
     def detect_task(self):
         cnt = 3
-        while self.task_run:
+        while self.task_run_enable:
             if self.task_run_flag == False:
                 time.sleep(1)
                 cnt = 3
@@ -376,12 +419,12 @@ class MyFrame(wx.Frame):
 
             if cnt != 0:
                 # 查询IMEI号
-                imei = self.get_cat1_imei()
-                self.label_imei_text.SetLabelText(imei)
+                self.get_cat1_imei()
+                self.label_imei_text.SetLabelText(self.imei)
 
                 # 查询ICCID
-                iccid = self.get_cat1_iccid()
-                self.label_iccid_text.SetLabelText(iccid)
+                self.get_cat1_iccid()
+                self.label_iccid_text.SetLabelText(self.iccid)
 
                 # 查询版本号
                 self.get_cat1_version()
@@ -406,19 +449,21 @@ class MyFrame(wx.Frame):
             wx.MessageBox("串口不能相同", "错误", wx.OK | wx.ICON_ERROR)
             return
 
+        # 清空界面文本显示内容
+        self.label_csq_text.SetLabelText("")
+        self.label_imei_text.SetLabelText("")
+        self.label_iccid_text.SetLabelText("")
+        self.label_sw_vertion_text.SetLabelText("")
+        self.label_result_text.SetLabelText("")
+
         if self.button.GetLabelText() == "开始检测":
             self.button.SetLabelText("停止检测")
-            self.label_csq_text.SetLabelText("")
-            self.label_imei_text.SetLabelText("")
-            self.label_iccid_text.SetLabelText("")
-            self.label_sw_vertion_text.SetLabelText("")
-            self.label_result_text.SetLabelText("")
         else:
+            self.button.SetLabelText("开始检测")
+
+            self.task_run_flag = False
             self.sc1.stop_serial_threads()
             self.sc2.stop_serial_threads()
-            self.task_run_flag = False
-            # self.task_thread.join()
-            self.button.SetLabelText("开始检测")
             self.sc1.close_serial_port(self.ser1)
             self.sc2.close_serial_port(self.ser2)
             self.statusbar.SetStatusText(self.com1_name + " 未连接", 0)
@@ -426,25 +471,152 @@ class MyFrame(wx.Frame):
             return
 
         self.ser1 = self.sc1.open_serial_port(self.com1_name, 115200)
+        self.ser2 = self.sc2.open_serial_port(self.com2_name, 9600)
+
+        if not (self.ser1 and self.ser2):
+            self.sc1.close_serial_port(self.ser1)
+            self.sc2.close_serial_port(self.ser2)
+            self.button.SetLabelText("开始检测")
+            if not self.ser1:
+                wx.MessageBox("串口1 " + self.com1_name + " 打开失败", "错误", wx.OK | wx.ICON_ERROR)
+            else:
+                wx.MessageBox("串口2 " + self.com2_name + " 打开失败", "错误", wx.OK | wx.ICON_ERROR)
+            return
+
         if self.ser1 is not None:
             self.statusbar.SetStatusText(self.com1_name + " 已连接", 0)
             self.sc1.start_serial_threads(self.ser1)
         else:
             self.statusbar.SetStatusText(self.com1_name + " 连接失败", 0)
 
-        self.ser2 = self.sc2.open_serial_port(self.com2_name, 9600)
         if self.ser2 is not None:
             self.statusbar.SetStatusText(self.com2_name + " 已连接", 1)
             self.sc2.start_serial_threads(self.ser2)
         else:
             self.statusbar.SetStatusText(self.com2_name + " 连接失败", 1)
 
+        # 启动检测线程
         self.task_run_flag = True
-        if self.task_run == False:
-            self.task_run = True
+        if self.task_run_enable == False:
+            self.task_run_enable = True
             self.task_thread = threading.Thread(target=self.detect_task)
             self.task_thread.daemon = True
             self.task_thread.start()
+
+    # 恢复自动检测
+    def resume_auto_detect(self):
+        self.label_csq_text.SetLabelText("")
+        self.label_imei_text.SetLabelText("")
+        self.label_iccid_text.SetLabelText("")
+        self.label_sw_vertion_text.SetLabelText("")
+        self.label_result_text.SetLabelText("")
+
+        if self.ser1 and self.ser2:
+            self.sc1.resume_recv_data()
+            self.sc2.resume_recv_data()
+            self.task_run_flag = True
+
+    # 串口自动检测线程
+    def auto_detect_serial(self):
+        removeList = []
+        addList = []
+
+        while True:
+            current_ports = get_available_ports()
+            if self.ser_ports != current_ports:
+                removeList = [port for port in self.ser_ports if port not in current_ports]
+                if len(removeList) > 0:
+                    for port in removeList:
+                        print("detect serial remove: %s" % port)
+                        remove_index = []
+                        for i in range(self.cb1.GetCount()):
+                            if port[1] == self.cb1.GetString(i).split(" ")[0]:
+                                self.task_run_flag = False
+                                self.sc1.pause_recv_data()  # 任意一个串口插拔所有的都不接收数据
+                                self.sc2.pause_recv_data()  # 任意一个串口插拔所有的都不接收数据
+                                if port[1] == self.com1_name:
+                                    self.sc1.stop_serial_threads()
+                                    self.sc1.close_serial_port(self.ser1)
+                                remove_index.append(i)
+                                self.statusbar.SetStatusText("", 0)
+
+                        # 统一删除更新列表
+                        for index in remove_index:
+                            self.cb1.Delete(index)
+                        self.cb1.Refresh()
+
+                        remove_index = []
+                        for i in range(self.cb2.GetCount()):
+                            if port[1] == self.cb2.GetString(i).split(" ")[0]:
+                                self.task_run_flag = False
+                                self.sc1.pause_recv_data()  # 任意一个串口插拔所有的都不接收数据
+                                self.sc2.pause_recv_data()  # 任意一个串口插拔所有的都不接收数据
+                                if port[1] == self.com2_name:
+                                    self.sc2.stop_serial_threads()
+                                    self.sc2.close_serial_port(self.ser2)
+                                remove_index.append(i)
+                                self.statusbar.SetStatusText("", 1)
+
+                        # 统一删除更新列表
+                        for index in remove_index:
+                            self.cb2.Delete(index)
+                        self.cb2.Refresh()
+
+                addList = [port for port in current_ports if port not in self.ser_ports]
+                if len(addList) > 0:
+                    for port in addList:
+                        print("detect serial plug: %s" % port)
+                        if port[2].startswith("ASR"):
+                            if self.cb1.GetCount() == 0:
+                                self.statusbar.SetStatusText(self.com1_name + " 未连接", 0)
+
+                            self.cb1.Append(port[1] + "  " + port[2])
+                            self.cb1.SetSelection(0)
+
+                            if self.com1_name == "":
+                                self.com1_name = self.cb1.GetStringSelection().split(' ')[0]
+                                print("当前选择：%s\n com1_name: %s" % (self.cb1.GetStringSelection(), self.com1_name))
+
+                            for i in range(self.cb1.GetCount()):
+                                if self.com1_name == self.cb1.GetString(i).split(" ")[0] and self.com1_name == port[1]:
+                                    self.cb1.SetSelection(i)
+
+                                    if self.task_run_enable:
+                                        self.ser1 = self.sc1.open_serial_port(self.com1_name, 115200)
+                                        self.statusbar.SetStatusText(self.com1_name + " 已连接", 0)
+                                        self.resume_auto_detect()
+                                    else:
+                                        self.statusbar.SetStatusText(self.com1_name + " 未连接", 0)
+
+                            self.cb1.Refresh()
+                        else:
+                            if self.cb2.GetCount() == 0:
+                                self.statusbar.SetStatusText(self.com2_name + " 未连接", 1)
+
+                            self.cb2.Append(port[1] + "  " + port[2])
+                            self.cb2.SetSelection(0)
+                            
+                            if self.com2_name == "":
+                                self.com2_name = self.cb2.GetStringSelection().split(' ')[0]
+                                print("当前选择：%s\n com2_name: %s" % (self.cb2.GetStringSelection(), self.com2_name))
+
+                            for i in range(self.cb2.GetCount()):
+                                if self.com2_name == self.cb2.GetString(i).split(" ")[0] and self.com2_name == port[1]:
+                                    self.cb2.SetSelection(i)
+
+                                    if self.task_run_enable:
+                                        self.ser2 = self.sc2.open_serial_port(self.com2_name, 9600)
+                                        self.statusbar.SetStatusText(self.com2_name + " 已连接", 1)
+                                        self.resume_auto_detect()
+                                    else:
+                                        self.statusbar.SetStatusText(self.com2_name + " 未连接", 1)
+
+                            self.cb2.Refresh()
+
+            # 更新当前列表缓存
+            self.ser_ports = current_ports
+
+            time.sleep(2)
 
 
 def main():
