6558e96fab77c1aaf4da1b85554618350ca41ff6
wangzhen <wanghzhenk@fotile.com>
2025-02-07 15:02:58 +0800

    1. 增加gif图片生成脚本

---
+++ gen_gif.py
@@ -0,0 +1,48 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import imageio
+import os
+import sys
+
+def create_gif(source,name,duration):
+    '''
+    生成gif的函数, 原始图片仅支持png
+    source: 为png图片列表(排好序)
+    name: 生成的文件名称
+    duration: 每张图片之间的时间间隔
+    loop: 循环次数, 0为无限循环
+    '''   
+    # 读取PNG文件并创建GIF
+    with imageio.get_writer(name, mode='I', duration=duration, loop=0) as writer:  # duration为每帧间隔时间（秒）
+        for image_path in source:
+            image = imageio.imread(image_path)
+            writer.append_data(image)
+    print("%s 处理完成"%name)
+
+def gen_gif(or_path, output, fps=10):
+    '''
+    or_path: 目标的文件夹
+    '''
+    pic_list = sorted([os.path.join(or_path, f) for f in os.listdir(or_path) if f.endswith('.png')])
+    gif_name = output # 生成gif文件的名称
+    duration_time = 1/fps # (1/fps) 每秒帧数
+    # print(duration_time)
+    # 生成gif
+    create_gif(pic_list, gif_name, duration_time)
+
+if __name__ == '__main__':    
+    path='E:\\zzk_ota\\img\\png\\auto'
+    gen_gif(path, "auto.gif", 12)
+
+    path='E:\\zzk_ota\\img\\png\\bootanim'
+    gen_gif(path, "bootanim.gif", 12)
+
+    path='E:\\zzk_ota\\img\\png\\strong'
+    gen_gif(path, "strong.gif", 12)
+
+    path='E:\\zzk_ota\\img\\png\\wave'
+    gen_gif(path, "wave.gif", 12)
+
+    path='E:\\zzk_ota\\img\\png\\weak'
+    gen_gif(path, "weak.gif", 12)
