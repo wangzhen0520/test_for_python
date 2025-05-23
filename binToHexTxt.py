eb8939248a06c889ba367e5cdc1c6b95b01e67a1
wangzhen <wanghzhenk@fotile.com>
2025-05-23 17:53:41 +0800

    1. 增加tts合成调试

--- binToHexTxt.py
+++ binToHexTxt.py
@@ -17,31 +17,34 @@ def get_file_size(file_path):
     except Exception as e:
         return f"发生错误: {e}"
 
+
 def add_header_to_bin_file(input_file, output_file, header_data):
     # 定义头部结构
-    HEADER_FORMAT = '8sI'  # I: unsigned int (4 bytes), h: short (2 bytes), 10s: string of 10 bytes
+    # I: unsigned int (4 bytes), h: short (2 bytes), 10s: string of 10 bytes
+    HEADER_FORMAT = '8sI'
     HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
-    
+
     print("HEADER_SIZE: %d" % HEADER_SIZE)
 
     # 打开输入文件和输出文件
     with open(input_file, 'rb') as infile, open(output_file, 'wb') as outfile:
         # 打包头部数据
         header = struct.pack(HEADER_FORMAT, *header_data)
-        
+
         # 写入头部
         outfile.write(header)
-        
+
         # 读取原始文件内容并写入输出文件
         outfile.write(infile.read())
+
 
 def change_fonts_to_bin():
     input_file = 'E:\\share\\code\\fotile_project\\rtos_app_touch\\main\\app\\res_spi\\font\OPPOSanSR-sim.ttf'
     output_file = 'E:\\share\\code\\fotile_project\\rtos_app_touch\\main\\app\\res_spi\\font\\font.bin'
     input_file_size = get_file_size(input_file)
-    
+
     print("input_file_size: %d" % input_file_size)
-    
+
     # 示例头部数据
     header_data = (b'font', input_file_size)  # 整数、短整数和字符数组
 
@@ -51,8 +54,8 @@ def change_fonts_to_bin():
 
 def convert_bin_to_hex():
     # 将bin文件转换为hex文件, 并保存到指定路径, 并按照16进制格式输出
-    bin_file_path = 'E:\\zzk_ota\\ota\\all-test.bin'
-    hex_file_path = 'E:\\zzk_ota\\ota\\all-test.txt'
+    bin_file_path = 'C:\\Users\\wangzhenk\\Downloads\\test.mp3'
+    hex_file_path = 'C:\\Users\\wangzhenk\\Downloads\\test.mp3.txt'
 
     with open(bin_file_path, 'rb') as bin_file:
         bin_data = bin_file.read()
@@ -61,28 +64,29 @@ def convert_bin_to_hex():
         i = 0
         for byte in bin_data:
             i += 1
-            
+
             # if i > 3615 :
             #     while i <= 4096:
             #         hex_file.write('{:02x}'.format(255))
-                    
+
             #         if i % 16 == 0:
             #             hex_file.write(' \n')
             #         else:
             #             hex_file.write(' ')
-                        
+
             #         i += 1
-                
+
             hex_file.write('{:02x}'.format(byte))
 
-            if i % 16 == 0:
-                hex_file.write(' \n')
-                if (i % 1024) == 0:
-                    hex_file.write('\n')
-            else:
-                hex_file.write(' ')
+            # if i % 16 == 0:
+            #     hex_file.write(' \n')
+            #     if (i % 1024) == 0:
+            #         hex_file.write('\n')
+            # else:
+            #     hex_file.write(' ')
 
-    print(f'Bin file {bin_file_path} has been converted to Hex file {hex_file_path}')
+    print(
+        f'Bin file {bin_file_path} has been converted to Hex file {hex_file_path}')
 
 
 if __name__ == '__main__':
---
+++ tts_speech_tts.py
@@ -0,0 +1,122 @@
+# coding=utf-8
+
+'''
+requires Python 3.6 or later
+
+pip install asyncio
+pip install websockets
+
+'''
+
+import asyncio
+import uuid
+import json
+import requests
+import time
+import hmac
+import hashlib
+
+product_id = "279630209"
+product_key = "085757baadb96edbffcdc2f09ab68ab7"
+product_secret = "ef59258308d5691c39e07626e0e7a983"
+voice_id = "gdfanfp"
+api_reg_url = "https://auth.dui.ai/auth/device/register"
+api_tts_url = "https://tts.dui.ai/runtime/v2/synthesize"
+formate = "plain"
+device_name = "1C:79:2D:2F:B2:98"
+
+
+def hmac_sha1(key: bytes, message: bytes) -> str:
+    """
+    计算 HMAC-SHA1 签名
+    :param key: 密钥（字节类型）
+    :param message: 消息（字节类型）
+    :return: 十六进制格式的签名字符串
+    """
+    # 创建 HMAC-SHA1 对象
+    hmac_obj = hmac.new(key, message, hashlib.sha1)
+
+    # 返回十六进制字符串结果
+    return hmac_obj.hexdigest()
+
+
+def reg_device() -> str:
+    nonce = str(uuid.uuid4()).replace("-", "")
+    print("nonce: %s %d" % (nonce, len(nonce)))
+    timestamp = int(round(time.time() * 1000))
+    print("timestamp: %d" % timestamp)
+    sig_data = f"{product_key}{formate}{nonce}{product_id}{timestamp}"
+    print("sig_data: %s" % sig_data)
+
+    signature = hmac_sha1(product_secret.encode(
+        "utf-8"), sig_data.encode("utf-8"))
+    print("signature: %s" % signature)
+
+    url = f'{api_reg_url}?productKey={product_key}&format={formate}&productId={product_id}&timestamp={timestamp}&nonce={nonce}&sig={signature}'
+    print("url: %s" % url)
+
+    body = {
+        "platform": "linux",
+        "deviceName": device_name
+    }
+    payload_body = str.encode(json.dumps(body))
+
+    response = requests.post(url, data=payload_body, headers={
+        'Content-Type': 'application/json'})
+    print(response.text)
+    rsp_str = json.loads(response.text)
+    device_secret = rsp_str['deviceSecret']
+    print("device_secret: %s" % device_secret)
+    return device_secret
+
+
+def submit_tts(device_secret, text):
+    nonce = str(uuid.uuid4()).replace("-", "")
+    timestamp = int(round(time.time() * 1000))
+    sig_data = f"{device_name}{nonce}{product_id}{timestamp}"
+    signature = hmac_sha1(device_secret.encode(
+        "utf-8"), sig_data.encode("utf-8"))
+
+    body = {
+        "context": {
+            "productId": product_id,
+        },
+        "request": {
+            "requestId": nonce,
+            "audio": {
+                "audioType": "mp3",
+                "sampleRate": 16000
+            },
+            "tts": {
+                "text": text,
+                "textType": "text",
+                "voiceId": voice_id,
+                "speed": "1.0",
+                "volume": 100
+            }
+        }
+    }
+    url = f'{api_tts_url}?voiceId={voice_id}&deviceName={device_name}&nonce={nonce}&productId={product_id}&timestamp={timestamp}&sig={signature}'
+    print("url: %s" % url)
+
+    payload_body = str.encode(json.dumps(body))
+    response = requests.post(url, data=payload_body, headers={
+        'Content-Type': 'application/json'})
+    # print(response.text)
+    with open(f"{text}.mp3", "wb") as f:
+        f.write(response.content)
+        print(f"write file {text}.mp3 success")
+
+
+async def test_submit():
+    req_device_secret = reg_device()
+    text_list = ['网络不稳定,请稍后重试', '授权失败了,请重新授权', '网络离线了,我听不懂你说什么',
+                 '鉴权失败了,请重新授权', '响应超时了,暂时不能为您服务', '抱歉帮不了您, 有需要再叫我哦',
+                 '抱歉没听懂你说什么, 有需要再叫我哦']
+    for text in text_list:
+        submit_tts(req_device_secret, text)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test_submit())
